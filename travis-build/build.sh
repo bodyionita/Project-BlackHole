13d3228516e1d85f5f8d50dccabd257d57adeab1
Bogdan Ionita <bogdan.ionita96@gmail.com>
2019-04-19 07:44:45 +0100

    travis username and pass for unity

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -15,18 +15,20 @@ mkdir $UNITY_BUILD_DIR
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
+  -username "bionita.anglia@gmail.com" \
+  -password "BlackHole2019"
   -silent-crashes \
   -logFile \
   -projectPath "$PROJECT_PATH" \
-  -buildWindows64Player  "$(pwd)/build/win/ci-build.exe" \
+  -buildTarget "Android" \
   -quit \
   | tee "$LOG_FILE"
   
 if [ $? = 0 ] ; then
-  echo "Building Windows exe completed successfully."
+  echo "Building completed successfully."
   ERROR_CODE=0
 else
-  echo "Building Windows exe failed. Exited with $?."
+  echo "Building failed. Exited with $?."
   ERROR_CODE=1
 fi
 
